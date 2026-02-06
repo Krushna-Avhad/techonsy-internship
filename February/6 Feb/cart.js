4e7b1f14a44b101dc44d551402c6b45f9e5ec85b
Krushna Avhad <krushna07avhad@gmail.com>
2026-02-06 22:47:54 +0530

    Add main HTML structure for e-commerce page

---
+++ February/6 Feb/cart.js
@@ -0,0 +1,8 @@
+import { watch } from "./product.js"; //named import
+import mobile from "./product.js"; //default import
+
+
+
+// importing - 
+// named - curly braces and original name is mandatory
+// default - no curly braces and name can be changed, curly braces and original name is not mandatory 
---
+++ February/6 Feb/product.js
@@ -0,0 +1,25 @@
+export let watch = { //named export
+    cname:"titan",
+    price: 2000,
+    color:"black",
+    display:"led",
+    warranty:"2 years" 
+}
+
+export let mobile = { //named export
+    cname:"titan",
+    price: 5000,
+    color:"black",
+    display:"led",
+    warranty:"5 years" 
+}
+ 
+export default function demo() {  // default export
+    console.log("This is a demo function");
+}
+
+
+
+// export - 
+// named export - consists of multiple exports in a file
+// default export - consists of only one export in a file
