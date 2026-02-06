f2b7ce539f3457a642dd68b78683c09cc0056f51
Krushna Avhad <krushna07avhad@gmail.com>
2026-02-06 23:18:59 +0530

    Add shopping cart functionality

--- February/6 Feb/cart.js
+++ February/6 Feb/cart.js
@@ -3,6 +3,8 @@ import mobile from "./product.js"; //default import
 
 
 
+
+
 // importing - 
 // named - curly braces and original name is mandatory
 // default - no curly braces and name can be changed, curly braces and original name is not mandatory 
